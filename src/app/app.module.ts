bb663cc1ec0ef1a9588adf529ed97bc5b032a50d
Toanns2004 <toanns2004@gmail.com>
2022-12-26 10:59:31 +0700

    toan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {SentitemsComponent} from "./sent items/sentitems.component";
+import {RouterModule, Routes} from "@angular/router";
+import {FavoritesComponent} from "./Favorites/favorites.component";
+import {InboxComponent} from "./inbox/inbox.component";
 
+
+const appRoutes: Routes=[
+  {path: '',component:FavoritesComponent},
+  {path: 'sent items',component: SentitemsComponent},
+  {path: 'inbox',component: InboxComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,SentitemsComponent,FavoritesComponent,InboxComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sent items/sentitems.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-sent',
+  templateUrl: './sentitems.component.html',
+  styleUrls: ['./sentitems.component.css']
+})
+export class SentitemsComponent{
+
+}
